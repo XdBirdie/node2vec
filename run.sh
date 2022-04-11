ee2ee66b124923d3d0a4f63eac1b1451e3978423
XdBirdie <dcchen@stu.xidian.edu.cn>
2022-04-11 17:36:36 +0800

    Use the same partitioner

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ spark-submit --class com.navercorp.Main \
        ./target/node2vec-0.1.2-SNAPSHOT.jar\
        --cmd node2vec --directed false --indexed false --weighted false\
        --walkLength 20  --numWalks 5\
-       --input graph/BlogCatalog --output emb/Blog.emb > log/node2vec16-inv.log
+       --input graph/BlogCatalog --output emb/Blog.emb > log/test.log
