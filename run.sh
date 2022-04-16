44b4443256a1158a5f26c74755dec6d4507b7cab
XdBirdie <dcchen@stu.xidian.edu.cn>
2022-04-16 12:28:33 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,29 +1,31 @@
 # nohup shell/test.sh > log/disk.log &
 
-spark-submit --class com.navercorp.Main \
-       --master spark://sepc724.se.cuhk.edu.hk:7077\
-       ./target/node2vec-0.1.2-SNAPSHOT.jar\
-       --cmd node2vec --version baseline --directed false --indexed false --weighted false\
-       --walkLength 20  --numWalks 5 --degree 100\
-       --input graph/BlogCatalog --output emb/Blog.emb > log/blog_baseline1.log
+Dataset=graph/cit-Patents.txt
 
-spark-submit --class com.navercorp.Main \
-       --master spark://sepc724.se.cuhk.edu.hk:7077\
-       ./target/node2vec-0.1.2-SNAPSHOT.jar\
-       --cmd node2vec --version broadcast --directed false --indexed false --weighted false\
-       --walkLength 20  --numWalks 5 --degree 100\
-       --input graph/BlogCatalog --output emb/Blog.emb > log/blog_broadcast1.log
+# spark-submit --class com.navercorp.Main \
+#        --master spark://sepc724.se.cuhk.edu.hk:7077\
+#        ./target/node2vec-0.1.2-SNAPSHOT.jar\
+#        --cmd node2vec --version baseline --directed false --indexed false --weighted false\
+#        --walkLength 20  --numWalks 5 --degree 100\
+#        --input $Dataset --output emb/Blog.emb > log/blog_baseline1.log
+
+# spark-submit --class com.navercorp.Main \
+#        --master spark://sepc724.se.cuhk.edu.hk:7077\
+#        ./target/node2vec-0.1.2-SNAPSHOT.jar\
+#        --cmd node2vec --version broadcast --directed false --indexed false --weighted false\
+#        --walkLength 20  --numWalks 5 --degree 100\
+#        --input $Dataset --output emb/Blog.emb > log/blog_broadcast1.log
 
 spark-submit --class com.navercorp.Main \
        --master spark://sepc724.se.cuhk.edu.hk:7077\
        ./target/node2vec-0.1.2-SNAPSHOT.jar\
        --cmd node2vec --version join2 --directed false --indexed false --weighted false\
        --walkLength 20  --numWalks 5 --degree 100\
-       --input graph/BlogCatalog --output emb/Blog.emb > log/blog_join2-1.log
+       --input $Dataset --output emb/Blog.emb > log/blog_join2-1.log
 
-spark-submit --class com.navercorp.Main \
-       --master spark://sepc724.se.cuhk.edu.hk:7077\
-       ./target/node2vec-0.1.2-SNAPSHOT.jar\
-       --cmd node2vec --version partition --directed false --indexed false --weighted false\
-       --walkLength 20  --numWalks 5 --degree 100\
-       --input graph/BlogCatalog --output emb/Blog.emb > log/blog_partition1.log
+# spark-submit --class com.navercorp.Main \
+#        --master spark://sepc724.se.cuhk.edu.hk:7077\
+#        ./target/node2vec-0.1.2-SNAPSHOT.jar\
+#        --cmd node2vec --version partition --directed false --indexed false --weighted false\
+#        --walkLength 20  --numWalks 5 --degree 100\
+#        --input $Dataset --output emb/Blog.emb > log/blog_partition1.log
